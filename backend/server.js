c750c1b544b13a27fc240656859c55f85456ce1b
Bnmpp <boonaum.scy@gmail.com>
2025-04-14 01:06:47 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,22 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../Frontend')));
 
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/html/dashboard-admin.html'));
+    res.sendFile(path.join(__dirname, '../Frontend/html/dashboard-admin.html'));
 });
 
 app.get('/event-admin', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/html/event-admin.html'));
+    res.sendFile(path.join(__dirname, '../Frontend/html/event-admin.html'));
 });
 
 app.get('/reservation-admin', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/html/reservation-admin.html'));
+    res.sendFile(path.join(__dirname, '../Frontend/html/reservation-admin.html'));
 });
 
 app.get('/user-admin', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/html/user-admin.html'));
+    res.sendFile(path.join(__dirname, '../Frontend/html/user-admin.html'));
 });
 
 
